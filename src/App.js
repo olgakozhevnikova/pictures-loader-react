71143fd4dea11eb4c0aa0ff0dfda1717c289fbbe
Olga <oolya.kozhevnikova@gmail.com>
2018-09-01 17:55:26 +0530

    Display the date in a proper format

--- src/App.js
+++ src/App.js
@@ -20,10 +20,24 @@ class App extends Component {
     PicAPI.get().then(picture => {
       this.setState(prevState => ({
         pictures: [...prevState.pictures, picture],
-        date: Date()
+        date: this.getDate()
       }))
       console.log(this.state.date)
     })
+  }
+
+  // get the date and the time, when a picture was loaded
+  getDate() {
+    let date = new Date()
+    let dd = date.getDate()
+    let mm = date.getMonth() + 1
+    let yyyy = date.getFullYear()
+    let hh = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+
+    return yyyy + '-' + (mm < 10 ? '0' + mm : mm) + '-' + (dd < 10 ? '0' + dd : dd) +
+      ' ' + (hh < 10 ? '0' + hh : hh) + ':' + (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec)
   }
 
   render() {
